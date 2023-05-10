cf4bca84b72cc6b5a3e8929bdbb86b003ce4b57f
sreehari-nambiar <23268641+sreehari-nambiar@users.noreply.github.com>
2023-05-10 03:09:26 +0000

    Adding a new CLI functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -7,3 +7,10 @@ def wiki(name="War Goodness", length=1):
     """
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    '''
+    Search Wikipedia for Names
+    '''
+    results = wikipedia.search(name)
+    return results
