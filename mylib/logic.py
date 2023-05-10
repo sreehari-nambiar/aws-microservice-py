510c884847edd78382aa4f4ab62776534fa3c513
sreehari-nambiar <23268641+sreehari-nambiar@users.noreply.github.com>
2023-05-10 04:56:10 +0000

    adding fastAPI

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,4 +1,5 @@
 import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goodness", length=1):
@@ -8,9 +9,19 @@ def wiki(name="War Goodness", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
+
 def search_wiki(name):
-    '''
+    """
     Search Wikipedia for Names
-    '''
+    """
     results = wikipedia.search(name)
     return results
+
+
+def phrase(name):
+    """
+    Returns from Wikipedia
+    """
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
